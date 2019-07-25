6ee0e8dddf5d651b0e04c80decdaebe0cf5c73b1
dineshdsv3 <dineshdsv3@gmail.com>
2019-07-25 22:51:15 +0530

    ErrorMessage & FormValidation

--- js/app.js
+++ js/app.js
@@ -3,5 +3,17 @@
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
 
+const form = document.getElementById('form');
 
-    
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const currency = document.getElementById('currency').value;
+    const crypto = document.getElementById('cryptocurrency').value;
+
+    if(currency === '' || crypto === '') {
+        ui.printMessage('All the fields are mandatory','deep-orange darken-4 card-panel');
+        
+    }else{
+        console.log('Success');
+    }
+})
--- js/ui.js
+++ js/ui.js
@@ -9,7 +9,7 @@ class UI {
        cryptoAPI.getCryptoCurrenciesList()
         .then((data) => {
             let currencies = data.contextWrites.to.ico.finished;
-            console.log(currencies);
+            // console.log(currencies);
                 currencies.forEach((currency,ind) => {
                     if(ind<10){
                     const select = document.getElementById('cryptocurrency');
@@ -20,7 +20,19 @@ class UI {
                     }
                     
                 })
-            })
-            ;
+            });
+    }
+
+    printMessage(message,className) {
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        const messagesDiv = document.querySelector('.messages')
+        messagesDiv.appendChild(div);
+
+        // Remove after 3 secs
+        setTimeout(() => {
+            document.querySelector('.messages div').remove();
+        },3000)
     }
 }
